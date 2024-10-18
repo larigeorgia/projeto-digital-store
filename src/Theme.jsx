fda4473885ac4f3de5ba9cea7874725c7615c100
Lariça Geórgia <larigeorgiab@gmail.com>
2024-10-17 23:54:21 -0300

    criacao do GlobalStyles e Theme

---
+++ src/Theme.jsx
@@ -0,0 +1,15 @@
+const theme = {
+    colors: {
+        primary: '#C92071',
+        secondary: '#B5B6F2',
+        tertiary: '#991956',
+        error: '#EE4266',
+        success: '#52CA76',
+        warning: '#F6AA1C',
+    },
+    // size: {
+
+    // }
+}
+
+export default theme;
